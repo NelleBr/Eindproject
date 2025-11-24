c621be84a47827dcac06b2bdb2e9ba963ad00e38
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2025-11-24 10:56:23 +0100

    feat: add header with basic navigation

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volleybal Webshop</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>VolleyShop</h1>
+            <nav>
+                <div>
+                    <a href="#">Home</a>
+                    <a href="#">Shop</a>
+                    <a href="#">Over ons</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Login</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
+
+</html>
