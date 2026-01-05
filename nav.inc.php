b7e3f0369dbf7c280bc332ed5a252b3fc1b89608
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2026-01-05 15:22:11 +0100

    feat: show admin link in nav for admins

--- nav.inc.php
+++ nav.inc.php
@@ -7,6 +7,10 @@
                 <a href="producten.php">Producten</a>
                 <a href="cart.php">Winkelmandje</a>
                 <a href="account.php">Account</a>
+
+                <?php if (isset($_SESSION["is_admin"]) && $_SESSION["is_admin"] == 1): ?>
+                    <a href="admin.php">Admin</a>
+                <?php endif; ?>
             </div>
         </nav>
     </div>
