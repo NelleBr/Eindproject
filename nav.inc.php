ad386f8948c7c7cbe50b3778ac7510532c3e3226
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2026-01-05 15:03:17 +0100

    chore: move navigation to nav include file

---
+++ nav.inc.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <h1>VolleyShop</h1>
+        <nav>
+            <div>
+                <a href="index.php">Home</a>
+                <a href="producten.php">Producten</a>
+                <a href="cart.php">Winkelmandje</a>
+                <a href="account.php">Account</a>
+            </div>
+        </nav>
+    </div>
+</header>
