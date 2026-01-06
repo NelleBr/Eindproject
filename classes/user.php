530eb3930cc03bf18059e5bcd2683977a7354e77
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2026-01-06 19:57:25 +0100

    chore: added classes folder

---
+++ classes/user.php
@@ -0,0 +1,34 @@
+<?php
+
+class User
+{
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
