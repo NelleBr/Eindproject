7f9a729be7013d9f63fa77cdf325bc3700548911
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2026-01-07 19:59:31 +0100

    feat: show product extra options on product page

--- product.php
+++ product.php
@@ -1,17 +1,13 @@
 <?php
-
 session_start();
 
 include_once(__DIR__ . "/db.inc.php");
 include_once(__DIR__ . "/classes/product.php");
+include_once(__DIR__ . "/classes/productOptions.php");
 
 $id = "";
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
-}
-
-if (!is_numeric($id)) {
-    exit("ongeldig product");
 }
 
 if (!is_numeric($id)) {
@@ -25,6 +21,20 @@ if (!$product) {
     exit("Product niet gevonden");
 }
 
+$productOptionClass = new ProductOption();
+$options = $productOptionClass->getByProductId($conn, $product["id"]);
+
+$groupedOptions = [];
+foreach ($options as $opt) {
+    $name = $opt["option_name"];
+    $value = $opt["option_value"];
+
+    if (!isset($groupedOptions[$name])) {
+        $groupedOptions[$name] = [];
+    }
+
+    $groupedOptions[$name][] = $value;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -61,7 +71,34 @@ if (!$product) {
 
                         <p><?php echo htmlspecialchars($product["description"]); ?></p>
 
-                        <a href="cart.php?add=<?php echo $product["id"]; ?>" class="button">Toevoegen aan winkelmandje</a>
+                        <?php if (count($groupedOptions) > 0): ?>
+                            <h3>Opties</h3>
+
+                            <form action="cart.php" method="get">
+                                <input type="hidden" name="add" value="<?php echo $product["id"]; ?>">
+
+                                <?php foreach ($groupedOptions as $optionName => $values): ?>
+                                    <div class="form-group">
+                                        <label for="<?php echo htmlspecialchars($optionName); ?>">
+                                            <?php echo htmlspecialchars(ucfirst($optionName)); ?>
+                                        </label>
+
+                                        <select id="<?php echo htmlspecialchars($optionName); ?>"
+                                            name="option[<?php echo htmlspecialchars($optionName); ?>]">
+                                            <option value="">Kies <?php echo htmlspecialchars($optionName); ?></option>
+
+                                            <?php foreach ($values as $value): ?>
+                                                <option value="<?php echo htmlspecialchars($value); ?>">
+                                                    <?php echo htmlspecialchars($value); ?>
+                                                </option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                <?php endforeach; ?>
+
+                                <a href="cart.php?add=<?php echo $product["id"]; ?>" class="button">Toevoegen aan winkelmandje</a>
+                            </form>
+                        <?php endif ?>
                     </div>
                 </div>
             </div>
