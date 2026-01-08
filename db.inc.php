19e20545036831866063d396bb09e0b525c5d29d
Nelle Breugelmans <nellebreugelmans@MacBook-Pro-van-Nelle.local>
2026-01-08 09:01:11 +0100

    chore: improved database connection with PDO

--- db.inc.php
+++ db.inc.php
@@ -1,2 +1,16 @@
 <?php
-$conn = new PDO('mysql:host=localhost;dbname=eindwerk', "root", "");
+
+$host = "localhost";
+$dbname = "eindwerk";
+$user = "root";
+$pass = "";
+
+$conn = new PDO(
+    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+    $user,
+    $pass,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
